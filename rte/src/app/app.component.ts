43b9bd00f533a4be4dabaffe3ed00794774c9055
thussain <thussain@cinnova.com>
2022-11-06 01:47:58 +0500

    Edit added

---
+++ rte/src/app/app.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import * as DecoupledEditor from '@ckeditor/ckeditor5-build-classic';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  form = new FormControl('');
+  constructor() {}
+
+  trash: any;
+
+  public Editor = DecoupledEditor;
+
+  public onReady(editor) {
+    editor.ui
+      .getEditableElement()
+      .parentElement.insertBefore(
+        editor.ui.view.toolbar.element,
+        editor.ui.getEditableElement()
+      );
+  }
+}
---
+++ rte/src/app/app.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { CKEditorModule } from 'ng2-ckeditor';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, CKEditorModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
